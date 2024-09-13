a7c85ce24021fcdcbacae9ed2c2261452768598f
NhanNV <t18.arsenal@gmail.com>
2024-09-13 18:41:00 +0700

    http

--- src/apiRequests/auth.ts
+++ src/apiRequests/auth.ts
@@ -1,61 +1,61 @@
 import http from '@/lib/http'
 import {
-  LoginBodyType,
-  LoginResType,
+  // LoginBodyType,
+  // LoginResType,
   RegisterBodyType,
   RegisterResType,
-  SlideSessionResType
+  // SlideSessionResType
 } from '@/schemaValidations/auth.schema'
-import { MessageResType } from '@/schemaValidations/common.schema'
+// import { MessageResType } from '@/schemaValidations/common.schema'
 
 const authApiRequest = {
-  login: (body: LoginBodyType) => http.post<LoginResType>('/Authentication/login', body),
+  // login: (body: LoginBodyType) => http.post<LoginResType>('/Authentication/login', body),
   register: (body: RegisterBodyType) =>
     http.post<RegisterResType>('/Authentication/Register', body),
-  auth: (body: { sessionToken: string; expiresAt: string }) =>
-    http.post('/api/auth', body, {
-      baseUrl: ''
-    }),
-  logoutFromNextServerToServer: (sessionToken: string) =>
-    http.post<MessageResType>(
-      '/Authentication/logout',
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${sessionToken}`
-        }
-      }
-    ),
-  logoutFromNextClientToNextServer: (
-    force?: boolean | undefined,
-    signal?: AbortSignal | undefined
-  ) =>
-    http.post<MessageResType>(
-      '/Authentication/logout',
-      {
-        force
-      },
-      {
-        baseUrl: '',
-        signal
-      }
-    ),
-  slideSessionFromNextServerToServer: (sessionToken: string) =>
-    http.post<SlideSessionResType>(
-      '/Authentication/slide-session',
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${sessionToken}`
-        }
-      }
-    ),
-  slideSessionFromNextClientToNextServer: () =>
-    http.post<SlideSessionResType>(
-      '/Authentication/slide-session',
-      {},
-      { baseUrl: '' }
-    )
+  // auth: (body: { sessionToken: string; expiresAt: string }) =>
+  //   http.post('/api/auth', body, {
+  //     baseUrl: ''
+  //   }),
+  // logoutFromNextServerToServer: (sessionToken: string) =>
+  //   http.post<MessageResType>(
+  //     '/Authentication/logout',
+  //     {},
+  //     {
+  //       headers: {
+  //         Authorization: `Bearer ${sessionToken}`
+  //       }
+  //     }
+  //   ),
+  // logoutFromNextClientToNextServer: (
+  //   force?: boolean | undefined,
+  //   signal?: AbortSignal | undefined
+  // ) =>
+  //   http.post<MessageResType>(
+  //     '/Authentication/logout',
+  //     {
+  //       force
+  //     },
+  //     {
+  //       baseUrl: '',
+  //       signal
+  //     }
+  //   ),
+  // slideSessionFromNextServerToServer: (sessionToken: string) =>
+  //   http.post<SlideSessionResType>(
+  //     '/Authentication/slide-session',
+  //     {},
+  //     {
+  //       headers: {
+  //         Authorization: `Bearer ${sessionToken}`
+  //       }
+  //     }
+  //   ),
+  // slideSessionFromNextClientToNextServer: () =>
+  //   http.post<SlideSessionResType>(
+  //     '/Authentication/slide-session',
+  //     {},
+  //     { baseUrl: '' }
+  //   )
 }
 
 export default authApiRequest
--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,7 +1,9 @@
 import envConfig from '@/config'
 import { normalizePath } from '@/lib/utils'
-import { LoginResType } from '@/schemaValidations/auth.schema'
-import { redirect } from 'next/navigation'
+import { LoginResType, RegisterBodyType
+  // , RegisterResType 
+} from '@/schemaValidations/auth.schema'
+// import { redirect } from 'next/navigation'
 
 type CustomOptions = Omit<RequestInit, 'method'> & {
   baseUrl?: string | undefined
@@ -15,51 +17,51 @@ export type err = {
   message: string
 }
 
-type EntityErrorPayload = {
-  message: string
-  errors: {
-    field: string
-    message: string
-  }[]
-}
+// type EntityErrorPayload = {
+//   message: string
+//   errors: {
+//     field: string
+//     message: string
+//   }[]
+// }
 
-export class HttpError extends Error {
-  status: number
-  payload: {
-    message: string
-    [key: string]: any
-  }
-  constructor({ status, payload }: { status: number; payload: any }) {
-    super('Http Error')
-    this.status = status
-    this.payload = payload
-  }
-}
+// export class HttpError extends Error {
+//   status: number
+//   payload: {
+//     message: string
+//     [key: string]: any
+//   }
+//   constructor({ status, payload }: { status: number; payload: any }) {
+//     super('Http Error')
+//     this.status = status
+//     this.payload = payload
+//   }
+// }
 
-export class EntityError extends HttpError {
-  status: 422
-  payload: EntityErrorPayload
-  constructor({
-    status,
-    payload
-  }: {
-    status: 422
-    payload: EntityErrorPayload
-  }) {
-    super({ status, payload })
-    this.status = status
-    this.payload = payload
-  }
-}
+// export class EntityError extends HttpError {
+//   status: 422
+//   payload: EntityErrorPayload
+//   constructor({
+//     status,
+//     payload
+//   }: {
+//     status: 422
+//     payload: EntityErrorPayload
+//   }) {
+//     super({ status, payload })
+//     this.status = status
+//     this.payload = payload
+//   }
+// }
 
-let clientLogoutRequest: null | Promise<any> = null
+// let clientLogoutRequest: null | Promise<any> = null
 export const isClient = () => typeof window !== 'undefined'
 const request = async <Response>(
   method: 'GET' | 'POST' | 'PUT' | 'DELETE',
   url: string,
   options?: CustomOptions | undefined
 ) => {
-  let body: FormData | string | undefined = undefined
+  let body: FormData | string | undefined | RegisterBodyType = undefined
   if (options?.body instanceof FormData) {
     body = options.body
   } else if (options?.body) {
@@ -94,7 +96,7 @@ const request = async <Response>(
     headers: {
       ...baseHeaders,
       ...options?.headers
-    } as any,
+    } as HeadersInit,
     body,
     method
   })
@@ -106,40 +108,40 @@ const request = async <Response>(
   // Interceptor là nời chúng ta xử lý request và response trước khi trả về cho phía component
   if (!res.ok) {
     if (res.status === ENTITY_ERROR_STATUS) {
-      throw new EntityError(
-        data as {
-          status: 422
-          payload: EntityErrorPayload
-        }
-      )
+      // throw new EntityError(
+      //   data as {
+      //     status: 422
+      //     payload: EntityErrorPayload
+      //   }
+      // )
     } else if (res.status === AUTHENTICATION_ERROR_STATUS) {
-      if (isClient()) {
-        if (!clientLogoutRequest) {
-          clientLogoutRequest = fetch('/api/auth/logout', {
-            method: 'POST',
-            body: JSON.stringify({ force: true }),
-            headers: {
-              ...baseHeaders
-            } as any
-          })
-          try {
-            await clientLogoutRequest
-          } catch (error) {
-          } finally {
-            localStorage.removeItem('sessionToken')
-            localStorage.removeItem('sessionTokenExpiresAt')
-            clientLogoutRequest = null
-            location.href = '/login'
-          }
-        }
-      } else {
-        const sessionToken = (options?.headers as any)?.Authorization.split(
-          'Bearer '
-        )[1]
-        redirect(`/logout?sessionToken=${sessionToken}`)
-      }
+      // if (isClient()) {
+      //   if (!clientLogoutRequest) {
+      //     clientLogoutRequest = fetch('/api/auth/logout', {
+      //       method: 'POST',
+      //       body: JSON.stringify({ force: true }),
+      //       headers: {
+      //         ...baseHeaders
+      //       } as any
+      //     })
+      //     try {
+      //       await clientLogoutRequest
+      //     } catch (error) {
+      //     } finally {
+      //       localStorage.removeItem('sessionToken')
+      //       localStorage.removeItem('sessionTokenExpiresAt')
+      //       clientLogoutRequest = null
+      //       location.href = '/login'
+      //     }
+      //   }
+      // } else {
+      //   const sessionToken = (options?.headers as any)?.Authorization.split(
+      //     'Bearer '
+      //   )[1]
+      //   redirect(`/logout?sessionToken=${sessionToken}`)
+      // }
     } else {
-      throw new HttpError(data)
+      // throw new HttpError(data)
     }
   }
   // Đảm bảo logic dưới đây chỉ chạy ở phía client (browser)
@@ -174,19 +176,13 @@ const http = {
   ) {
     return request<Response>('POST', url, { ...options, body })
   },
-  put<Response>(
-    url: string,
-    body: any,
-    options?: Omit<CustomOptions, 'body'> | undefined
-  ) {
-    return request<Response>('PUT', url, { ...options, body })
-  },
-  delete<Response>(
-    url: string,
-    options?: Omit<CustomOptions, 'body'> | undefined
-  ) {
-    return request<Response>('DELETE', url, { ...options })
-  }
+  // put<Response>(
+  //   url: string,
+  //   body: any,
+  //   options?: Omit<CustomOptions, 'body'> | undefined
+  // ) {
+  //   return request<Response>('PUT', url, { ...options, body })
+  // },
 }
 
 export default http
